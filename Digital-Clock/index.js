93d7c4477c05552da1ba08f8e15ba33d7f664a06
Prasanta Dutta <82324631+Prasanta-Dutta@users.noreply.github.com>
2025-04-24 21:37:38 +0000

    Adding stopwatch functionalities but still have to improve many things. Define two interval id for time and stopwatch. Also need modification on stopwatch functions

--- Digital-Clock/index.js
+++ Digital-Clock/index.js
@@ -9,15 +9,27 @@ const timeBox = document.getElementById('time-box');
 const stopwatchBox = document.getElementById('stopwatch-box');
 const timerBox = document.getElementById('timer-box');
 
+const stopwatchStartBtn = document.getElementById('stopwatchStartBtn');
+const stopwatchStopBtn = document.getElementById('stopwatchStopBtn');
+const stopwatchResetBtn = document.getElementById('stopwatchResetBtn');
+const stopwatch = document.getElementById('stopwatch');
 
-let inervalID = setInterval(() => {
+
+let intervalID = setInterval(() => {
     let currTime = new Date();
-    showTime.innerHTML = currTime.toLocaleTimeString();
+    let hh = currTime.getHours().toString().padStart(2, "0");
+    let mm = currTime.getMinutes().toString().padStart(2, "0");
+    let ss = currTime.getSeconds().toString().padStart(2, "0");
+
+    let ampm = currTime.toLocaleTimeString('en-IN').split(' ')[1];
+
+    showTime.innerHTML = `${hh}:${mm}:${ss} ${ampm}`;
 }, 1000);
 
 
 stopwatchBar.addEventListener("click", (e) => {
-    clearInterval(inervalID);
+    clearInterval(intervalID);
+    intervalID = null;
     timeBar.classList.remove('active');
     timeBox.classList.add('d-none');
 
@@ -29,6 +41,8 @@ stopwatchBar.addEventListener("click", (e) => {
 });
 
 timeBar.addEventListener("click", (e) => {
+    clearInterval(intervalID);
+    intervalID = null;
     
     stopwatchBar.classList.remove('active');
     stopwatchBox.classList.add('d-none');
@@ -39,8 +53,61 @@ timeBar.addEventListener("click", (e) => {
     timeBox.classList.remove('d-none');
     timeBar.classList.add('active');
 
-    inervalID = setInterval(() => {
+    let hh, mm, ss, ampm;   //  Better if define inside setInterval(), bcs garbage collected after every second.
+
+    intervalID = setInterval(() => {
         let currTime = new Date();
-        showTime.innerHTML = currTime.toLocaleTimeString();
+        // showTime.innerHTML = currTime.toLocaleTimeString();
+        hh = currTime.getHours().toString().padStart(2, "0");
+        mm = currTime.getMinutes().toString().padStart(2, "0");
+        ss = currTime.getSeconds().toString().padStart(2, "0");
+
+        ampm = currTime.toLocaleTimeString('en-IN').split(' ')[1];
+
+        showTime.innerHTML = `${hh}:${mm}:${ss} ${ampm}`;
     }, 1000);
+});
+
+stopwatchStartBtn.addEventListener("click", (e) => {
+    let startTimeStamp = Date.now();
+    let tillNowTimeStamp;
+    let displayTime;
+
+    let time = stopwatch.textContent.split(':');
+
+    stopwatchStartBtn.classList.add('d-none');
+    stopwatchStopBtn.classList.remove('d-none');
+
+
+    intervalID = setInterval(() => {
+        tillNowTimeStamp = Date.now();
+        displayTime = new Date(tillNowTimeStamp-startTimeStamp).getSeconds();  //   Time in seconds 
+        // displayTime = Math.floor((tillNowTimeStamp - startTimeStamp) / 1000);
+        console.log(displayTime);
+
+        let hh = parseInt(time[0]);
+        let mm = parseInt(time[1]);
+        let ss = parseInt(time[2]);   //  After every second garbage collected
+
+        displayTime = displayTime + (hh*3600) + (mm*60) + ss;
+        hh = Math.floor(displayTime/3600).toString();
+        mm = Math.floor((displayTime%3600) / 60).toString();
+        ss = Math.floor(displayTime%60).toString();
+        stopwatch.innerHTML = `${hh.padStart(2,"0")}:${mm.padStart(2,"0")}:${ss.padStart(2,"0")}`;
+    }, 1000);
+})
+
+
+stopwatchResetBtn.addEventListener("click", (e) => {
+    clearInterval(intervalID);
+    intervalID = null;
+    stopwatch.innerHTML = "00:00:00";
+});
+
+stopwatchStopBtn.addEventListener("click", (e) => {
+    clearInterval(intervalID);
+    intervalID = null;
+
+    stopwatchStartBtn.classList.remove('d-none');
+    stopwatchStopBtn.classList.add('d-none');    
 });
