0c402796f34c6ca911ee7735f6a9a89dbb142b4e
Prasanta Dutta <82324631+Prasanta-Dutta@users.noreply.github.com>
2025-04-26 08:56:45 +0000

    Progress-Bar completed. User gives the progress percentage and it will change progress value.

--- Progress-Bar/index.js
+++ Progress-Bar/index.js
@@ -1,7 +1,6 @@
 const progressValue = document.getElementById('progress-value');
 const progressBar = document.getElementById('progress-bar');
-
-// console.log(progressValue)
+const progressBarInner = document.getElementById('progress-bar-inner');
 
 progressValue.addEventListener("keypress", (e) => {
     // e.preventDefault();
@@ -10,8 +9,21 @@ progressValue.addEventListener("keypress", (e) => {
         console.log(progressPercentage, typeof progressPercentage);
 
         if(progressPercentage === null || progressPercentage === '' || isNaN(progressPercentage)){
-            progressValue.innerHTML = "Enter valid Value";
-            console.log("hi")
+            progressValue.placeholder = "Enter valid Value";
+            progressValue.value = "";
+        }
+        else if(progressPercentage >= 0 && progressPercentage <= 100){
+            let width = progressBar.offsetWidth;
+            
+            console.log(width, typeof width);
+            width = width*progressPercentage/100;
+            console.log(width, typeof width);
+            progressBarInner.style.width = `${width}px`;
+            progressBarInner.innerHTML = `${progressPercentage}%`;
+        }
+        else{
+            progressValue.placeholder = "Percentage b/w 0-100";
+            progressValue.value = "";
         }
     }
 });
